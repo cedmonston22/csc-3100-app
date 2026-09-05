12b12e3e7c39f63813d1ac6a84c043a31ba73a10
cedmonst22 <cedmonst@calpoly.edu>
2026-09-05 16:24:49 -0700

    IE2: Step 3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,9 +6,39 @@ const port = 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(users);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "McDonalds Founder",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
