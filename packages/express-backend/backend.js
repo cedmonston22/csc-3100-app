4373f43456605056ee6ad472954b59c3559fc111
cedmonst22 <cedmonst@calpoly.edu>
2026-09-05 16:34:26 -0700

    IE2: Step 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,3 +42,18 @@ const users = {
     },
   ],
 };
+
+const findUserByName = (name) => {
+    return users["users_list"].filter((user) => user["name"] == name);
+};
+
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result};
+        res.send(result);
+    }else{
+        res.send(users);
+    }
+});
