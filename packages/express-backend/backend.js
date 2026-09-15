25e58b290229a0d5bccb08d93a9264c146208afe
cedmonst22 <cedmonst@calpoly.edu>
2026-09-15 11:39:57 -0700

    IE3: 201 Content Created Backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -72,7 +72,7 @@ const addUser = (user) => {
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
-  res.send();
+  res.status(201).send();
 });
 
 const deleteUserById = (id) => {
